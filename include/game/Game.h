f7cd7ff32646833b97e2da8c8db876e7affd5209
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-09-19 15:04:15 +0300

    Game and GameService is initialized

---
+++ include/game/Game.h
@@ -0,0 +1,8 @@
+//
+// Created by Özgürhan Polat on 19.09.2024.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+#endif //GAME_H
---
+++ include/game/GameService.h
@@ -0,0 +1,8 @@
+//
+// Created by Özgürhan Polat on 19.09.2024.
+//
+
+#ifndef GAMESERVICE_H
+#define GAMESERVICE_H
+
+#endif //GAMESERVICE_H
