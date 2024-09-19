9c614618ef74f14786d28210c8bbac669c261a4b
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-09-19 22:10:25 +0300

    game is initialized with header files

--- include/game/Game.h
+++ include/game/Game.h
@@ -4,5 +4,35 @@
 
 #ifndef GAME_H
 #define GAME_H
+#include <vector>
 
+#include "../word/Word.h"
+using namespace std;
+using namespace word;
+
+namespace game {
+
+    class Game {
+        vector<Word> words;
+        int numberOfWords;
+        //auto createdAt = std::chrono::system_clock::now();
+        auto createdAt;
+        int remainingWords;
+
+
+        public:
+            Game(vector<Word> &words);
+
+        // Words Getter
+        vector<Word> &getWords();
+
+        // Number of Words Getter
+        int getNumberOfWords();
+
+        // Number of Remaining Words
+        int getRemainingWords();
+
+
+    };
+}
 #endif //GAME_H
--- include/game/GameService.h
+++ include/game/GameService.h
@@ -4,5 +4,20 @@
 
 #ifndef GAMESERVICE_H
 #define GAMESERVICE_H
+#include "Game.h"
 
+using namespace std;
+using namespace game;
+
+namespace gameService {
+
+    class GameService {
+        public:
+            GameService();
+            Game &startAGame();
+            void playTheGame(Game &game);
+            void showResults(Game &game);
+
+    };
+}
 #endif //GAMESERVICE_H
